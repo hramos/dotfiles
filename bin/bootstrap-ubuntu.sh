e6f25bd22b6f21a3cea9e3503270bad9ec791f5a
Héctor Ramos <hector@hectorramos.com>
2019-03-05 17:03:40 +0000

    Fix up vim script

--- bin/bootstrap-ubuntu.sh
+++ bin/bootstrap-ubuntu.sh
@@ -6,6 +6,8 @@ echo "Setting up .bash_profile"
 touch $HOME/.bash_profile
 echo "source $HOME/dotfiles/user_profile" >> $HOME/.bash_profile
 
+GIT_REPOSITORY_PATH="$HOME/git"
+
 echo "Setting up git"
 if ! [ -x "$(command -v git)" ]; then
   echo 'Error: git is not installed.' >&2
@@ -15,18 +17,24 @@ git config --global user.name "Héctor Ramos"
 git config --global user.email hector@hectorramos.com
 
 echo "Pulling down remote repositories..."
-cd ~
-mkdir git
-cd git
-git clone git@github.com:facebook/react-native.git
-git clone git@github.com:facebook/react-native-website.git
-cd ~
+if [ ! -d "$GIT_REPOSITORY_PATH" ]; then
+  mkdir $GIT_REPOSITORY_PATH 
+fi
+
+if [ ! -d "$GIT_REPOSITORY_PATH/react-native" ]; then
+  git clone git@github.com:facebook/react-native.git $GIT_REPOSITORY_PATH/react-native
+  (cd $GIT_REPOSITORY_PATH/react-native && git remote add fork git@github.com:hramos/react-native.git)
+fi
+if [ ! -d "$GIT_REPOSITORY_PATH/react-native-website" ]; then
+  git clone git@github.com:facebook/react-native-website.git $GIT_REPOSITORY_PATH/react-native-website
+  (cd $GIT_REPOSITORY_PATH/react-native-website && git remote add fork git@github.com:hramos/react-native-website.git)
+fi
+if [ ! -d "$GIT_REPOSITORY_PATH/react-native-bot" ]; then
+  git clone git@github.com:hramos/react-native-bot.git $GIT_REPOSITORY_PATH/react-native-bot
+fi
 
 # Set up vim
-./bin/vim-setup.sh
-
-# Install applications
-sudo dpkg -i bat_0.10.0_amd64.deb
+source $HOME/dotfiles/bin/vim-setup.sh
 
 echo "Done!"
 
